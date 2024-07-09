cfeeaa017432e28d59e40118b04d43be9601b820
Deadbeef <ent3rm4n@gmail.com>
2024-07-09 15:57:21 +0800

    gamma correction

--- src/color.rs
+++ src/color.rs
@@ -11,13 +11,21 @@ impl Vec3Token for ColorToken {
 
 pub type Color = Vec3<ColorToken>;
 
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        linear_component.sqrt()
+    } else {
+        0.0
+    }
+}
+
 impl Color {
     pub fn write_to(self, out: &mut impl Write) -> io::Result<()> {
         let Vec3(r, g, b) = self;
         let intensity = Interval::new(0.000, 0.999);
-        let rbyte = (256.0 * intensity.clamp(r)) as u64;
-        let gbyte = (256.0 * intensity.clamp(g)) as u64;
-        let bbyte = (256.0 * intensity.clamp(b)) as u64;
-        writeln!(out, "{rbyte} {gbyte} {bbyte}")
+        let [r, g, b] = [r, g, b]
+            .map(linear_to_gamma)
+            .map(|x| (256.0 * intensity.clamp(x)) as u64);
+        writeln!(out, "{r} {g} {b}")
     }
 }
